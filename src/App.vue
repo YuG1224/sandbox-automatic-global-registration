f9952e3a79ac6e8184099245f8ea2ca9e93b1899
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2021-10-21 01:34:09 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <base-component-a :msg="msg" />
+    <base-component-b :msg="msg" />
+    <base-component-c :msg="msg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BaseComponentB.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="foo">{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.foo {
+  color: blue;
+}
+</style>
